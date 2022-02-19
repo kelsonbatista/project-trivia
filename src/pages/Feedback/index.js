ab4fd53445f19c6145c406f7278d21b66415734e
kelsonbatista <kelsonbatista@gmail.com>
2022-02-19 19:14:03 -0300

    Requisito 12

--- src/pages/Feedback/index.js
+++ src/pages/Feedback/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import TableApp from '../../components/Table';
 
-function Feedback() {
+function Feedback(props) {
+  const { player: { name, score, gravatarEmail } } = props;
   return (
     <section className="feedback">
       <h1>Feedback</h1>
-      <h2 data-testid="feedback-text">Message</h2>
+      <TableApp
+        name={ name }
+        score={ score }
+        gravatarEmail={ gravatarEmail }
+      />
+      <h3 data-testid="feedback-text">Message</h3>
     </section>
   );
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  player: PropTypes.object,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  player: state.player,
+});
+
+export default connect(mapStateToProps, null)(Feedback);
