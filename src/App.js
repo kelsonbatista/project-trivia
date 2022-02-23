af6adb402e6bacfc10a76a2319ea324721fa84b1
kelsonbatista <kelsonbatista@gmail.com>
2022-02-23 12:40:06 -0300

    Video renderizado

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './assets/images/logo.png';
+import localVideo from './assets/videos/intro.mp4';
 import './App.css';
 import Login from './pages/Login';
 
@@ -7,6 +8,11 @@ function App() {
   return (
     <div>
       <header>
+        <div className="video-container">
+          <video autoPlay muted loop>
+            <source src={ localVideo } type="video/mp4" />
+          </video>
+        </div>
         <section className="login">
           <img src={ logo } className="login__logo" alt="logo" />
           <div className="login__glow">&nbsp;</div>
