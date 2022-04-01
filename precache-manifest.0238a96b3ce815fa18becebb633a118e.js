3369de3170ce5753ceff4093787ea8e7f9359392
kelsonbatista <kelsonbatista@gmail.com>
2022-04-01 18:37:29 -0300

    Updates

---
+++ precache-manifest.0238a96b3ce815fa18becebb633a118e.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03b37e927714d6a5b95916de8dc85e40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4735aa762dfbdb12324",
+    "url": "/static/css/main.1f68241b.chunk.css"
+  },
+  {
+    "revision": "a3a12b654f31394dc1d6",
+    "url": "/static/js/2.9acba334.chunk.js"
+  },
+  {
+    "revision": "9eb45884572d2d19eb9671e4a0a8ccc1",
+    "url": "/static/js/2.9acba334.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4735aa762dfbdb12324",
+    "url": "/static/js/main.3475efce.chunk.js"
+  },
+  {
+    "revision": "f8a4e7c1de7ec49c0042",
+    "url": "/static/js/runtime-main.e2209b7b.js"
+  },
+  {
+    "revision": "92a91aa1c7b590c266937989f15310f5",
+    "url": "/static/media/bg_main.9d9b0ac1.jpg"
+  },
+  {
+    "revision": "8755157a05b63b3e1e01b76e2565e82c",
+    "url": "/static/media/bg_ranking3.76c8224c.jpg"
+  },
+  {
+    "revision": "bd3b92a064081405035427723509d470",
+    "url": "/static/media/certa-a-resposta.bd3b92a0.mp3"
+  },
+  {
+    "revision": "26e2d6247129c3d666b6d19ac1cb5be9",
+    "url": "/static/media/certa-a-resposta3.26e2d624.mp3"
+  },
+  {
+    "revision": "26417997126b37be3752efd1389db629",
+    "url": "/static/media/certa-a-resposta4.26417997.mp3"
+  },
+  {
+    "revision": "13346dbb000f9b5c25b593fefca8249f",
+    "url": "/static/media/controle-emocional.13346dbb.mp3"
+  },
+  {
+    "revision": "4b7ebd7fa82c17b60f50c050555a5e05",
+    "url": "/static/media/da-branco.4b7ebd7f.mp3"
+  },
+  {
+    "revision": "5c1e3d16aa10fdacb6306510d367a3ca",
+    "url": "/static/media/da-para-acertar.5c1e3d16.mp3"
+  },
+  {
+    "revision": "e9c324fe8cd7bc2665ce107dba467554",
+    "url": "/static/media/ele-treina.e9c324fe.mp3"
+  },
+  {
+    "revision": "04087624deff26edc5b5b52c4c98012f",
+    "url": "/static/media/entendeu-a-pergunta.04087624.mp3"
+  },
+  {
+    "revision": "eab6138620371f02c67a6fa018f34070",
+    "url": "/static/media/entrada.eab61386.mp3"
+  },
+  {
+    "revision": "cc9cdd5fe85a33575aa43c601608ce7a",
+    "url": "/static/media/essa-me-pegou.cc9cdd5f.mp3"
+  },
+  {
+    "revision": "01a1d0edf55b24724602cbef46fd7bb3",
+    "url": "/static/media/fofoca.01a1d0ed.mp3"
+  },
+  {
+    "revision": "ec7a86fb44cab6355c26e478775d3af2",
+    "url": "/static/media/ja-ouviu-falar.ec7a86fb.mp3"
+  },
+  {
+    "revision": "175585bbb91a82dfcce9896fe085ba88",
+    "url": "/static/media/logo.de9da511.png"
+  },
+  {
+    "revision": "234ad67a6cb2d0daaaec065c9c698e58",
+    "url": "/static/media/nao-eh-dificil.234ad67a.mp3"
+  },
+  {
+    "revision": "0bfe1651ba6a91488fec833b4425adf1",
+    "url": "/static/media/nao-eh-dificil2.0bfe1651.mp3"
+  },
+  {
+    "revision": "03e1626db293eb0f9e1282765bc1593c",
+    "url": "/static/media/nao1.03e1626d.mp3"
+  },
+  {
+    "revision": "b95edf85a56dfc181ac4a7e86aeb5e72",
+    "url": "/static/media/nao2.b95edf85.mp3"
+  },
+  {
+    "revision": "143559f76c0d739e92c1e8dcd669050a",
+    "url": "/static/media/nao3.143559f7.mp3"
+  },
+  {
+    "revision": "4226b7cbf7dc9d5f3bf6035abb3bb365",
+    "url": "/static/media/nao4.4226b7cb.mp3"
+  },
+  {
+    "revision": "32f19a1c25f1266ac2bdec408f58ac04",
+    "url": "/static/media/nervosismo.32f19a1c.mp3"
+  },
+  {
+    "revision": "20e8648ec8cd827ba5fdf7c689d52311",
+    "url": "/static/media/nervosismo2.20e8648e.mp3"
+  },
+  {
+    "revision": "fddd6467a42323d20828ecdf584a67fd",
+    "url": "/static/media/opcoes4.fddd6467.mp3"
+  },
+  {
+    "revision": "481a410b179c7adec13dc0ebf3de9524",
+    "url": "/static/media/pergunta-boa.481a410b.mp3"
+  },
+  {
+    "revision": "a33044ec181d81129849a8dc80bac05a",
+    "url": "/static/media/portioli1.e9d42f2f.jpg"
+  },
+  {
+    "revision": "480d197b8758f2aca5b4ba190ab3406f",
+    "url": "/static/media/portioli2.9a104cf7.jpg"
+  },
+  {
+    "revision": "217d14cc637059c7d230db286cb5d83f",
+    "url": "/static/media/portioli3.41c32e49.jpg"
+  },
+  {
+    "revision": "6eea59fbd529cbe378f202bb9e7ddc8d",
+    "url": "/static/media/portioli4.b9e8ea29.jpg"
+  },
+  {
+    "revision": "002feb135b3c200590288c544b5e2cfc",
+    "url": "/static/media/portioli5.2675c435.jpg"
+  },
+  {
+    "revision": "50f0548fde5e5b20d9ff4d38577ade04",
+    "url": "/static/media/portioli6.da031f52.jpg"
+  },
+  {
+    "revision": "cbb0aeb3ed1ff2899da2d72eba8f6b38",
+    "url": "/static/media/portioli7.01763409.jpg"
+  },
+  {
+    "revision": "6d49b9ca16bc7895553bb5ddd90412a8",
+    "url": "/static/media/posso-perguntar.6d49b9ca.mp3"
+  },
+  {
+    "revision": "cef6e9e75cf9749d6235e26f33895b40",
+    "url": "/static/media/posso-perguntar2.cef6e9e7.mp3"
+  },
+  {
+    "revision": "414684aee77379ee6d161df5c47d3f2a",
+    "url": "/static/media/sera-que-hoje.414684ae.mp3"
+  },
+  {
+    "revision": "19015e269fc1a0874d1af4d6e254d6da",
+    "url": "/static/media/sera-que-hoje2.19015e26.mp3"
+  },
+  {
+    "revision": "19287826e629594ef762b8b3fd00891b",
+    "url": "/static/media/ta-indo-bem.19287826.mp3"
+  },
+  {
+    "revision": "efbef1700afd6b5dab30802ab6a53d63",
+    "url": "/static/media/ta-por-dentro.efbef170.mp3"
+  },
+  {
+    "revision": "9421b8d3778298cadedb5cc158a4bcd8",
+    "url": "/static/media/vai-chutar.9421b8d3.mp3"
+  },
+  {
+    "revision": "37b17b2e6254857afb84ca75a9a527b9",
+    "url": "/static/media/voce-pode-acertar.37b17b2e.mp3"
+  }
+]);
